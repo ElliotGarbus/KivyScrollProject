4b8534df765e913a89aa640b0009cb7ce0a28615
ElliotGarbus <elliotg2@cox.net>
2025-10-06 20:34:36 -0700

    updated todo list

--- nested_scrollview_manager.py
+++ nested_scrollview_manager.py
@@ -28,6 +28,7 @@ Scrollbar scrolling behavior:
 - For parallel scrollviwes the inner scrollbar does not propagate scroll to the outer scrollview.
 """
 
+# TODO: Test scroll events with nested scrollviews.
 # TODO: Evaluate integration of the Manager in the ScrollView.
 # TODO: create feature, dwelling on a non-button widget can be turned into a scroll.
 # TODO: create a test suite for the updated ScrollView & NSVM for the kivy test suite.
